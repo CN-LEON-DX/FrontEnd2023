59f8deab63a82fc18d4a7f11d1b2fd668027c7ea
FuzzilFox144 <125853450+CN-LEON-DX@users.noreply.github.com>
2023-12-30 15:44:27 +0700

    30/12/2023
    No please

---
+++ Fontend/Lesson3/a.cpp
@@ -0,0 +1,32 @@
+// Viết nhật kí là một việc làm rất hay, nó giúp ta lưu giữ lại những dòng suy nghĩ, những cảm xúc của bản thân. Khi đọc lại những dòng nhật kí đó cũng giúp ta nhiều điều, biết được rằng thì ra trước kia mình muốn làm cái này, muốn làm cái kia, mục tiêu, ước mơ, hoài bão,… Những suy nghĩ khi ấy bây giờ đây là đọc lại, đôi khi giúp ta lấy lại được những cảm xúc, những động lực thuở mới ban đầu, để rồi tiếp tục cố gắng theo đuổi một mục tiêu nào đó…
+
+// Tổng kết cuối năm, Trường đọc lại nhật kí của mình và thấy bản thân mình tuy có trưởng thành hơn một chút, nhưng vẫn còn quá lười biếng và có những mục tiêu chưa hoàn thành, vì thế anh ấy quyết định sẽ phải cố gắng nhiều hơn, chăm chỉ nhiều hơn trong năm tới để không phải hối hận và tiếc nuối. Đọc lại nhật kí Trường thấy rằng trong nhật kí của anh ấy chỉ viết thời gian là ngày tháng năm, và quên không viết thứ. Bây giờ Trường hỏi Giang ngày DD tháng MM năm 2023 là thứ mấy để viết thêm vào nhật kí. Trường hỏi Giang rất nhiều, nên Giang đôi khi cảm thấy mệt mỏi. Vì thế hãy giúp Giang bằng việc viết chương trình để trả lời câu hỏi của Trường một cách nhanh chóng và chính xác.
+
+// Giang cho bạn biết là ngày 1 tháng 1 năm 2023 là chủ nhật.
+
+// Đầu vào
+// Dòng đầu tiên chứa số nguyên T (1<=T<=1000) - số trường hợp thử nghiệm.
+// Trong T dòng tiếp theo, mỗi dòng chưa 3 chứa nguyên DD, MM, YYYY – tương đương với ngày tháng năm tương ứng (đảm bảo rằng dữ liệu đầu vào chính xác và không có số không đứng đầu, YYYY=2023)
+
+// Đầu ra
+// in ra T dòng Thứ trong tuần tương ứng vào dữ liệu được nhập vào.
+// Nếu là thứ hai in ra T_HAI
+// Nếu là thứ ba in ra T_BA
+// Nếu là thứ tư in ra T_TU
+// Nếu là thứ năm in ra T_NAM
+// Nếu là thứ sáu in ra T_SAU
+// Nếu là thứ bảy in ra T_BAY
+// Nếu là chủ nhật in ra CHU_NHAT
+
+// Input
+// Copy
+// 3
+// 1 1 2023
+// 28 2 2023
+// 31 12 2023
+// Ouput
+
+// Copy
+// CHU_NHAT
+// T_BA
+// CHU_NHAT
